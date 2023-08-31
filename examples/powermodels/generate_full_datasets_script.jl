d7ee477708815bf914e0ffe3484f452d236248e5
Andrew David Werner Rosemberg <arosemberg3@login-phoenix-slurm-4.pace.gatech.edu>
2023-08-31 19:37:16 -0400

    update dataset gen

--- examples/powermodels/generate_full_datasets_script.jl
+++ examples/powermodels/generate_full_datasets_script.jl
@@ -7,7 +7,7 @@ using Test
 using UUIDs
 using PowerModels
 using Clarabel
-using JuMP.MOI
+import JuMP.MOI as MOI
 import ParametricOptInterface as POI
 
 cached = MOI.Bridges.full_bridge_optimizer(
@@ -32,13 +32,14 @@ filetype = ArrowFile
 case_name = "pglib_opf_case300_ieee"
 network_formulation = SOCWRConicPowerModel
 case_file_path = joinpath(path, case_name)
-solver = () -> POI.Optimizer(cached)
+solver = () -> POI.Optimizer(cached())
 
 # Generate dataset
 success_solves = 0.0
 for i in 1:num_batches
     _success_solves, number_variables, number_loads, batch_id = generate_dataset_pglib(case_file_path, case_name; 
         num_p=num_p, filetype=filetype, network_formulation=network_formulation, solver=solver,
+        load_sampler= (_o, n) -> load_sampler(_o, n, max_multiplier=1.25, min_multiplier=0.8, step_multiplier=0.01)
     )
     success_solves += _success_solves
 end
