af464efb505be08d23f00363cb0c7a70519cfd7d
Andrew Rosemberg <andrewrosemberg@gmail.com>
2023-09-17 18:11:10 -0400

    Update PowerModels Iterator (#7)
    * test soc generation
    
    * update dataset gen
    
    ---------
    
    Co-authored-by: Andrew David Werner Rosemberg <arosemberg3@login-phoenix-slurm-3.pace.gatech.edu>
    Co-authored-by: Andrew David Werner Rosemberg <arosemberg3@login-phoenix-slurm-4.pace.gatech.edu>

--- examples/powermodels/generate_full_datasets_script.jl
+++ examples/powermodels/generate_full_datasets_script.jl
@@ -6,6 +6,17 @@ using L2O
 using Test
 using UUIDs
 using PowerModels
+using Clarabel
+import JuMP.MOI as MOI
+import ParametricOptInterface as POI
+
+cached = MOI.Bridges.full_bridge_optimizer(
+    MOI.Utilities.CachingOptimizer(
+        MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
+        Clarabel.Optimizer(),
+    ),
+    Float64,
+)
 
 # Paths
 path_powermodels = joinpath(pwd(), "examples", "powermodels")
@@ -13,20 +24,22 @@ path = joinpath(path_powermodels, "data")
 include(joinpath(path_powermodels, "pglib_datagen.jl"))
 
 # Parameters
-num_batches = 2
-num_p = 1000
+num_batches = 1
+num_p = 10
 filetype = ArrowFile
 
 # Case name
-case_name = "pglib_opf_case5_pjm"
-network_formulation = DCPPowerModel
+case_name = "pglib_opf_case300_ieee"
+network_formulation = SOCWRConicPowerModel
 case_file_path = joinpath(path, case_name)
+solver = () -> POI.Optimizer(cached())
 
 # Generate dataset
 success_solves = 0.0
 for i in 1:num_batches
     _success_solves, number_variables, number_loads, batch_id = generate_dataset_pglib(case_file_path, case_name; 
-        num_p=num_p, filetype=filetype, network_formulation=network_formulation
+        num_p=num_p, filetype=filetype, network_formulation=network_formulation, solver=solver,
+        load_sampler= (_o, n) -> load_sampler(_o, n, max_multiplier=1.25, min_multiplier=0.8, step_multiplier=0.01)
     )
     success_solves += _success_solves
 end
