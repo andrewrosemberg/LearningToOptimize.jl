33059d6827056d1487112e7223dae2afe56cbd71
Andrew Rosemberg <arosemberg3@gatech.edu>
2023-09-18 09:27:58 -0400

    update code

--- test/worst_case.jl
+++ test/worst_case.jl
@@ -5,41 +5,18 @@ using JuMP
 using Dualization
 import JuMP.MOI as MOI
 
-function _moi_set_upper_bound(
-    moi_backend,
-    idx::MOI.VariableIndex,
-    upper,
-)
-    new_set = MOI.LessThan(upper)
-
-    JuMP._moi_add_constraint(moi_backend, idx, new_set)
-
-    return
-end
-
-function _moi_set_lower_bound(
-    moi_backend,
-    idx::MOI.VariableIndex,
-    lower,
-)
-    new_set = MOI.GreaterThan(lower)
-
-    JuMP._moi_add_constraint(moi_backend, idx, new_set)
-
-    return
-end
-
 # Load data
 case_name = "pglib_opf_case5_pjm"
 matpower_case_name = case_name * ".m"
-network_data = pglib(matpower_case_name)
+network_data_original = pglib(matpower_case_name)
+network_data = deepcopy(network_data_original)
 
 # Case Parameters
 network_formulation=DCPPowerModel
 original_load = [l["pd"] for l in values(network_data["load"])]
 max_total_load = sum(original_load) * 1.1
 
-# Create model
+# Create primal model
 solver = Ipopt.Optimizer
 model = Model(solver)
 load_var = @variable(model, load_var[i=1:length(original_load)])
@@ -57,11 +34,6 @@ pm = instantiate_model(
     jump_model=model,
 )
 
-# Solve the model
-# JuMP.optimize!(model)
-
-# optimal_cost = JuMP.objective_value(model)
-
 # Dualize the model
 dual_st = Dualization.dualize(JuMP.backend(model), 
     variable_parameters = load_moi_idx
@@ -69,11 +41,6 @@ dual_st = Dualization.dualize(JuMP.backend(model),
 
 dual_model = dual_st.dual_model
 primal_dual_map = dual_st.primal_dual_map
-
-# for (i,l) in enumerate(load_var)
-#     _moi_set_upper_bound(dual_model, primal_dual_map.primal_parameter[l.index], max_total_load)
-#     _moi_set_lower_bound(dual_model, primal_dual_map.primal_parameter[l.index], 0.0)
-# end
 
 jump_dual_model = JuMP.Model()
 map_moi_to_jump = MOI.copy_to(JuMP.backend(jump_dual_model), dual_model)
@@ -109,6 +76,33 @@ optimal_dual_cost = JuMP.objective_value(jump_dual_model)
 
 optimal_loads = value.(load_var_dual)
 
-cost_diff = (optimal_dual_cost - optimal_cost) / optimal_cost
-
 load_diff = sum(abs.(optimal_loads - original_load)) ./ sum(original_load)
+
+# Create final primal model
+network_data = deepcopy(network_data_original)
+model = Model(solver)
+for (i, l) in enumerate(values(network_data["load"]))
+    l["pd"] = optimal_loads[i]
+end
+
+# Instantiate the model
+pm = instantiate_model(
+    network_data,
+    network_formulation,
+    PowerModels.build_opf;
+    setting=Dict("output" => Dict("duals" => true)),
+    jump_model=model,
+)
+
+# Solve the model
+JuMP.optimize!(model)
+
+# Check the solution
+optimal_final_cost = JuMP.objective_value(model)
+termination_status = JuMP.termination_status(model)
+solution_primal_status = JuMP.primal_status(model)
+solution_dual_status = JuMP.dual_status(model)
+termination_status == MOI.INFEASIBLE && @error("Optimal solution not found")
+solution_primal_status != MOI.FEASIBLE_POINT && @error("Primal solution not found")
+solution_dual_status != MOI.FEASIBLE_POINT && @error("Dual solution not found")
+isapprox(optimal_final_cost, optimal_dual_cost; rtol=1e-4) || @warn("Final cost is not equal to dual cost")
