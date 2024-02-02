ad7fff1aadfca63115991ef80c41e99b8df44b29
Andrew Rosemberg <arosemberg3@gatech.edu>
2024-02-02 12:01:31 -0500

    uncomment

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,16 +35,16 @@ include(joinpath(test_dir, "inconvexhull.jl"))
     test_flux_jump_basic()
     test_inconvexhull()
 
-    # mktempdir() do path
-    #     test_problem_iterator(path)
-    #     test_worst_case_problem_iterator(path)
-    #     file_in, file_out = test_pglib_datasetgen(path, "pglib_opf_case5_pjm", 20)
-    #     file_in, file_out = test_generate_worst_case_dataset(
-    #         path, "pglib_opf_case5_pjm", 20
-    #     )
-    #     file_in, file_out = test_generate_worst_case_dataset_Nonconvex(
-    #         path, "pglib_opf_case5_pjm", 20
-    #     )
-    #     test_flux_forecaster(file_in, file_out)
-    # end
+    mktempdir() do path
+        test_problem_iterator(path)
+        test_worst_case_problem_iterator(path)
+        file_in, file_out = test_pglib_datasetgen(path, "pglib_opf_case5_pjm", 20)
+        file_in, file_out = test_generate_worst_case_dataset(
+            path, "pglib_opf_case5_pjm", 20
+        )
+        file_in, file_out = test_generate_worst_case_dataset_Nonconvex(
+            path, "pglib_opf_case5_pjm", 20
+        )
+        test_flux_forecaster(file_in, file_out)
+    end
 end
