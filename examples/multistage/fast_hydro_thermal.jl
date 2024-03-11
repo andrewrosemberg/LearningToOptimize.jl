46a9837600ac381cbac6b55041fea9d28d19d7a0
Andrew Rosemberg <arosemberg3@gatech.edu>
2024-03-11 13:16:19 -0400

    update

--- examples/multistage/fast_hydro_thermal.jl
+++ examples/multistage/fast_hydro_thermal.jl
@@ -24,35 +24,50 @@ function build_fast_hydro_thermal_multistage(; num_stages=2)
     state_params_in = Vector{Vector{VariableRef}}(undef, 2)
     state_params_out = Vector{Vector{VariableRef}}(undef, 2)
     uncertainty_samples = Vector{Dict{VariableRef, Vector{Float64}}}(undef, 2)
-    decision_rules = Vector{Any}(undef, 2)
     
     for t in 1:num_stages
         subproblems[t], state_params_in[t], state_params_out[t], uncertainty_params = fast_hydro_thermal_subproblem()
-        decision_rules[t] = Dense(2, 1)
         uncertainty_samples[t] = Dict(uncertainty_params .=> (t == 1 ? [[6.0]] : [[2.0, 10]]))
     end
 
-    return subproblems, state_params_in, state_params_out, uncertainty_samples, decision_rules
+    return subproblems, state_params_in, state_params_out, uncertainty_samples
 end
 
-function sample(uncertainty_samples::Dict{VariableRef, Vector{Float64}})
-    return Dict((k => v[rand(1:end)]) for (k, v) in uncertainty_samples)
-end
-
-sample(uncertainty_samples::Vector{Dict{VariableRef, Vector{Float64}}}) = [sample(uncertainty_samples[t]) for t in 1:length(uncertainty_samples)]
-
-function test_simulate_multistage(; num_samples=10)
-    subproblems, state_params_in, state_params_out, uncertainty_samples, decision_rules = build_fast_hydro_thermal_multistage()
+function test_simulate_multistage(model = Dense(2, 1); num_samples=10)
+    subproblems, state_params_in, state_params_out, uncertainty_samples = build_fast_hydro_thermal_multistage()
     initial_state = [0.0]
-    objective_values = [simulate_multistage(
-        subproblems, state_params_in, state_params_out, initial_state, 
-        decision_rules, 
-        sample(uncertainty_samples)
-    ) for _ in 1:num_samples]
+    objective_values = Array{Float64}(undef, num_samples)
+    for i in 1:num_samples
+        uncertainty_sample = sample(uncertainty_samples)
+        states = simulate_states(
+            initial_state, 
+            uncertainty_sample, 
+            [model for _ in 1:2]
+        )
+        objective_values[i] = simulate_multistage(
+            subproblems, state_params_in, state_params_out, states, 
+            uncertainty_sample
+        )
+    end
     return objective_values
 end
 
+using ChainRulesTestUtils
+
+function test_rrule_simulate_multistage(model = Dense(2, 1))
+    subproblems, state_params_in, state_params_out, uncertainty_samples = build_fast_hydro_thermal_multistage()
+    initial_state = [0.0]
+    test_rrule(simulate_multistage,
+        subproblems, state_params_in, state_params_out, initial_state, 
+        [model for _ in 1:2], 
+        sample(uncertainty_samples)
+    )
+end
+test_rrule_simulate_multistage()
 
 # Test
+using Statistics
 objective_values = test_simulate_multistage()
 mean(objective_values)
+
+train_multistage(Dense(2, 1), [0.0], build_fast_hydro_thermal_multistage()...)
--- examples/multistage/simulate_multistage.jl
+++ examples/multistage/simulate_multistage.jl
@@ -2,50 +2,46 @@ using JuMP
 import ParametricOptInterface as POI
 using HiGHS
 using Flux
+using Zygote
+using ChainRulesCore
+import ChainRulesCore.rrule
 
-"""
-    simulate_multistage(
-        subproblems::Vector{JuMP.Model},
-        state_params_in::Vector{Vector{VariableRef}},
-        state_params_out::Vector{Vector{VariableRef}},
-        initial_state::Vector{Float64},
-        decision_rules::Vector{Function},
-        Uncertainties::Vector{Dict{VariableRef, Float64}},
-    )
+function simulate_states(
+    initial_state::Vector{Float64},
+    uncertainties::Vector{Dict{VariableRef, Float64}},
+    decision_rules::Vector{F},
+) where {F}
+    num_stages = length(uncertainties)
+    states = Vector{Vector{Float64}}(undef, num_stages + 1)
+    states[1] = initial_state
+    for stage in 1:num_stages
+        uncertainties_stage = uncertainties[stage]
+        decision_rule = decision_rules[stage]
+        states[stage + 1] = decision_rule([states[stage]; collect(values(uncertainties_stage))])
+    end
+    return states
+end
 
-Simulate a multistage optimization problem.
-
-# Arguments
-- `subproblems::Vector{JuMP.Model}`: Vector of subproblems
-- `state_params_in::Vector{Vector{VariableRef}}`: Vector of state parameters for each stage
-- `state_params_out::Vector{Vector{VariableRef}}`: Vector of state parameters for each stage
-- `initial_state::Vector{Float64}`: Initial state
-- `decision_rules::Vector{Function}`: Vector of decision rules for each stage
-- `Uncertainties::Vector{Dict{VariableRef, Float64}}`: Vector of uncertainties for each stage
-"""
 function simulate_multistage(
     subproblems::Vector{JuMP.Model},
     state_params_in::Vector{Vector{VariableRef}},
     state_params_out::Vector{Vector{VariableRef}},
-    initial_state::Vector{Float64},
-    decision_rules::Vector{F},
+    states::Vector{Vector{Float64}},
     uncertainties::Vector{Dict{VariableRef, Float64}},
-    ) where {F}
-    
-    # Initialize state
-    state = initial_state
+    )
     
     # Loop over stages
     objective_value = 0.0
     for stage in 1:length(subproblems)
-        
+        state_in = states[stage]
+        state_out = states[stage + 1]
         # Get subproblem
         subproblem = subproblems[stage]
         
         # Update state parameters
         state_param_in = state_params_in[stage]
         for (i, state_var) in enumerate(state_param_in)
-            MOI.set(subproblem, POI.ParameterValue(), state_var, state[i])
+            MOI.set(subproblem, POI.ParameterValue(), state_var, state_in[i])
         end
         
         # Update uncertainty
@@ -54,14 +50,10 @@ function simulate_multistage(
             MOI.set(subproblem, POI.ParameterValue(), uncertainty_param, uncertainty_value)
         end
         
-        # Get decisions and transition state
-        decision_rule = decision_rules[stage]
-        state = decision_rule([state; collect(values(uncertainties_stage))])
-        
         # Update state parameters out
         state_param_out = state_params_out[stage]
         for (i, state_var) in enumerate(state_param_out)
-            MOI.set(subproblem, POI.ParameterValue(), state_var, state[i])
+            MOI.set(subproblem, POI.ParameterValue(), state_var, state_out[i])
         end
         
         # Solve subproblem
@@ -73,4 +65,57 @@ function simulate_multistage(
     
     # Return final objective value
     return objective_value
+end
+
+pdual(v::VariableRef) = MOI.get(JuMP.owner_model(v), POI.ParameterDual(), v)
+pdual(vs::Vector{VariableRef}) = [pdual(v) for v in vs]
+
+# Define rrule of simulate_multistage
+function rrule(::typeof(simulate_multistage), subproblems, state_params_in, state_params_out, states, uncertainties)
+    y = simulate_multistage(subproblems, state_params_in, state_params_out, states, uncertainties)
+    function _pullback(Δy)
+        pull_back_model = pdual.(state_params_in).+pdual.(state_params_out) * Δy
+        return (NoTangent(), NoTangent(), NoTangent(), NoTangent(), pull_back_model, NoTangent())
+    end
+    return y, _pullback
+end
+
+function sample(uncertainty_samples::Dict{VariableRef, Vector{Float64}})
+    return Dict((k => v[rand(1:end)]) for (k, v) in uncertainty_samples)
+end
+
+sample(uncertainty_samples::Vector{Dict{VariableRef, Vector{Float64}}}) = [sample(uncertainty_samples[t]) for t in 1:length(uncertainty_samples)]
+
+function train_multistage(model, initial_state, subproblems, state_params_in, state_params_out, uncertainty_samples; num_train_samples=100, optimizer=Flux.Adam(0.01))
+    num_stages = length(subproblems)
+    # Initialise the optimiser for this model:
+    opt_state = Flux.setup(optimizer, model)
+
+    for _ in 1:num_train_samples
+        # Sample uncertainties
+        uncertainty_sample = sample(uncertainty_samples)
+
+        # Calculate the gradient of the objective
+        # with respect to the parameters within the model:
+        models = Vector{Any}(undef, num_stages)
+        grads = Flux.gradient(model) do m
+            for (i, subproblem) in enumerate(subproblems)
+                models[i] = m
+            end
+            states = simulate_states(
+                initial_state, uncertainty_sample, 
+                models
+            )
+            simulate_multistage(
+                subproblems, state_params_in, state_params_out, states, 
+                uncertainty_sample
+            )
+        end
+
+        # Update the parameters so as to reduce the objective,
+        # according the chosen optimisation rule:
+        Flux.update!(opt_state, model, grads[1])
+    end
+    
+    return model
 end
